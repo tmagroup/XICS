5c04de9cfe7a9ff33c8f0f2f2921cd304e017f38
ConnectUs Developer <connectusdemo12@gmail.com>
2020-09-14 20:02:47 +0530

    database file update in git

--- application/config/database.php
+++ application/config/database.php
@@ -70,9 +70,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'live';
+$active_group = 'dev';
 // $active_group = 'default'; // comment for live
 $query_builder = TRUE;
+
+$db['dev'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'xics_live',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
 
 $db['live'] = array(
 	'dsn'	=> '',
