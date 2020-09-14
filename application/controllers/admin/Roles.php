c3c5a94e711c7f9c86312fd9b904c27283d3ab1b
ConnectUs Developer <connectusdemo12@gmail.com>
2020-09-14 21:48:37 +0530

    roles controller update in live

--- application/controllers/admin/Roles.php
+++ application/controllers/admin/Roles.php
@@ -8,7 +8,7 @@ class Roles extends Admin_controller
         if(get_user_role()!='user' && get_user_id()==1){
             redirect(site_url('admin/permission/denied/'));
         }
-        $this->load->model('Role_model');        
+        $this->load->model('Role_model');
     }
 
     /* Permissions Set by Role */
@@ -17,45 +17,46 @@ class Roles extends Admin_controller
         if(!$GLOBALS['role_permission']['view']){
             access_denied('role');
         }
-        
+
         //Submit Page
         if ($this->input->post()) {
-            $post = $this->input->post();         
-			$roleid = $post['userrole'];	
-			$response = $this->Role_model->updatePermission($post, $roleid); 
+            $post = $this->input->post();
+			$roleid = $post['userrole'];
+
+			$response = $this->Role_model->updatePermission($post, $roleid);
 			if (is_numeric($response) && $response>0) {
-				set_alert('success', sprintf(lang('updated_successfully'),lang('page_role')));				
+				set_alert('success', sprintf(lang('updated_successfully'),lang('page_role')));
 				//redirect(site_url('admin/roles/'));
 			}
 			else{
 				set_alert('danger', $response);
-			}	
-			
-			$data['role'] = $post;               
+			}
+
+			$data['role'] = $post;
         }
-        
-        
+
+
         //******************** Initialise ********************/
         //Roles
         $data['roles'] = $this->Role_model->get('');
-        $data['roles'] = dropdown($data['roles'],'roleid','name');        
+        $data['roles'] = dropdown($data['roles'],'roleid','name');
         //******************** End Initialise ********************/
-        
-        
+
+
         $data['title'] = lang('page_roles');
         $this->load->view('admin/roles/index', $data);
-    }   
-    
+    }
+
     public function ajax($id){
-        
+
         //******************** Initialise ********************/
         //Role
         $data['role'] = (array) $this->Role_model->get($id);
-        
+
         //Permissions
         $data['permissions'] = $this->Role_model->get_permissions();
         //******************** End Initialise ********************/
-        
+
         $this->load->view('admin/roles/tab-permissions', $data);
     }
 }
